4f2b352723d2670e9c0cd5d9f4ae043eaf7a8ac2
Laura Ruiz Muñoz <laura_r.m@hotmail.com>
2017-11-30 16:54:05 +0100

    Add files via upload

--- Pract6/Circuito.cpp
+++ Pract6/Circuito.cpp
@@ -1,19 +1,25 @@
-//PRÁCTICA 2 DE SISTEMAS GRÁFICOS INTERACTIVOS
+//PRÁCTICA 6 DE SISTEMAS GRÁFICOS INTERACTIVOS
 //Laura Ruiz Muñoz
 #define PROYECTO "Estrella de David"
 
 #include <iostream>
 #include <cmath>
-#include <GL\freeglut.h>
+#include <gl\freeglut.h>
 #include <Utilidades.h>
 #include <cmath>
+#include <stdlib.h>
+#include <sstream> 
 
-using namespace std;
+
 static GLuint tramo_1, tramo_2, tramo_3, tramo_4, tramo_5, tramo_6, tramo_7,tramo_8 ,tramo_9 ,tramo_10 ,tramo_11 ,tramo_12 ,tramo_13 ,tramo_14 ,tramo_15 ,tramo_16 ;
 GLfloat v0[3] = { 2,0,0 }, v1[3] = { -2,0,0 }, v2[3] = { -2,0,20}, v3[3] = { 2,0,17 },  v4[3] = { 6,0,22 }, v5[3] = { 2,0,25 }, v7[3] = { 2,0,35 }, v6[3] = {6,0,37};
 GLfloat v8[3] = { 0,0,43 }, v9[3] = { -1,0,38 }, v10[3] = { -14,0,38 }, v11[3] = { -18,0,43 }, v12[3] = { -18,0,0 }, v13[3] = { -14,0,0 }, v14[3] = { -14,0,-20 }, v15[3] = {-18,0,-17};
 GLfloat v16[3] = { -22,0,-22 }, v17[3] = { -18,0,-25 }, v18[3] = { -18,0,-35 }, v19[3] = { -22,0,-37 }, v20[3] = { -16,0,-43 }, v21[3] = { -15,0,-38 }, v22[3] = { -2,0,-38 }, v23[3] = {2,0,-43};
 GLfloat v24[3] = { 2,0,-21 }, v25[3] = { -2,0,-19 }, v26[3] = { 0,0,-16 }, v27[3] = { 4,0,-18 }, v28[3] = { 4,0,-9 }, v29[3] = { 0,0,-11 }, v30[3] = { -2,0,-7 }, v31[3] = {2,0,-5};
+static float pos_x = 0, pos_z = 0, look_x = 0, look_z = 1, velocity = 0, grad = 0;
+static int N = 10, M = 10;
+
+using namespace std;
 
 void init()
 {
@@ -128,6 +134,8 @@ void init()
 	glPolygonMode(GL_FRONT, GL_LINE);
 	quad(v31, v30, v1, v0, 3, 10);
 	glEndList();
+
+	
 }
 
 void display()
@@ -139,10 +147,11 @@ void display()
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	gluLookAt(0, 1, -16, 0, 1, -15, 0, 1, 0);
+	gluLookAt(pos_x, 1, pos_z, look_x, 1, look_z, 0, 1, 0);
 
 	ejes();
-
+	glPushMatrix();
+	glRotatef(180, 0, 0, 1);
 	glCallList(tramo_1);
 	glCallList(tramo_2);
 	glCallList(tramo_3);
@@ -159,8 +168,10 @@ void display()
 	glCallList(tramo_14);
 	glCallList(tramo_15);
 	glCallList(tramo_16);
+	glPopMatrix();
 
 	glFlush();
+	glutSwapBuffers();
 }
 
 void reshape(GLint w, GLint h)
@@ -173,15 +184,62 @@ void reshape(GLint w, GLint h)
 	gluPerspective(45, razon, 1, 30);
 }
 
+void moverVehiculo()
+{
+	static int horaAnterior = glutGet(GLUT_ELAPSED_TIME);
+	int horaActual = glutGet(GLUT_ELAPSED_TIME);
+	float tiempoTranscurrido = horaActual - horaAnterior;
+	pos_x -= velocity*(tiempoTranscurrido / 1000.0)*sin(grad*PI / 180.0);
+	pos_z -= velocity*(tiempoTranscurrido / 1000.0)*cos(grad*PI / 180.0);
+	look_z = pos_z - cos(grad*PI / 180.0);
+	look_x = pos_x - sin(grad*PI / 180.0);
+	horaAnterior = horaActual;
+}
+
+void onSpecialKey(int key, int x, int y)
+{
+	stringstream cuentakms;
+	switch (key)
+	{
+	case GLUT_KEY_UP: velocity += 0.2;
+		break;
+	case GLUT_KEY_LEFT: grad += 0.35;
+		break;
+	case GLUT_KEY_DOWN: velocity = max(0, velocity - 0.2);
+		break;
+	case GLUT_KEY_RIGHT: grad -= 0.35;
+		break;
+	}
+	cuentakms << "Interfaz de conducción. SPEED " << velocity << " m/s";
+	glutSetWindowTitle(cuentakms.str().c_str());
+	moverVehiculo();
+	glutPostRedisplay();
+}
+void idle()
+{
+	moverVehiculo();
+	glutPostRedisplay();
+}
+
+void onKey(unsigned char tecla, int x, int y) {
+	switch (tecla) {
+		case 27:
+			exit(0);
+			break;
+	}
+	glutPostRedisplay();
+}
+
 void main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB| GLUT_DEPTH);
 	glutInitWindowSize(600, 400);
-	glutCreateWindow(PROYECTO);
+	glutCreateWindow("Interfaz de conducción. SPEED 0 m/s");
 	init();
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
-	cout << PROYECTO << " en marcha " << endl;
+	glutSpecialFunc(onSpecialKey);
+	glutIdleFunc(idle);
 	glutMainLoop();
 }
