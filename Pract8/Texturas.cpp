896301767b27db05ee30151ab65276dac9467bba
Laura Ruiz Muñoz <laura_r.m@hotmail.com>
2017-12-17 03:32:14 +0100

    Con pancarta de Dio

--- Pract8/Texturas.cpp
+++ Pract8/Texturas.cpp
@@ -11,16 +11,18 @@
 #include <sstream> 
 #include <freeimage/FreeImage.h>
 
-static GLuint circuito;
+static GLuint circuito, pancartadio, estrella;
 GLfloat v0[3] = { 2,0,0 }, v1[3] = { -2,0,0 }, v2[3] = { -2,0,20 }, v3[3] = { 2,0,17 }, v4[3] = { 6,0,22 }, v5[3] = { 2,0,25 }, v7[3] = { 2,0,35 }, v6[3] = { 6,0,37 };
 GLfloat v8[3] = { 0,0,43 }, v9[3] = { -1,0,38 }, v10[3] = { -14,0,38 }, v11[3] = { -18,0,43 }, v12[3] = { -18,0,0 }, v13[3] = { -14,0,0 }, v14[3] = { -14,0,-20 }, v15[3] = { -18,0,-17 };
 GLfloat v16[3] = { -22,0,-22 }, v17[3] = { -18,0,-25 }, v18[3] = { -18,0,-35 }, v19[3] = { -22,0,-37 }, v20[3] = { -16,0,-43 }, v21[3] = { -15,0,-38 }, v22[3] = { -2,0,-38 }, v23[3] = { 2,0,-43 };
 GLfloat v24[3] = { 2,0,-21 }, v25[3] = { -2,0,-19 }, v26[3] = { 0,0,-16 }, v27[3] = { 4,0,-18 }, v28[3] = { 4,0,-9 }, v29[3] = { 0,0,-11 }, v30[3] = { -2,0,-7 }, v31[3] = { 2,0,-5 };
+//GLfloat v31[3]={-18, 0,50}, v32[3]={}
 static float velocity = 0, grad = 0;
 static float pos_x = 0, pos_z = 0, look_x = 0, look_z = 1;
 static int N = 10, M = 10;
 static enum { ALAMBRICO, SOLIDO, DOUBLE } modo;
 GLuint tex0;
+GLuint tex1, tex2;
 
 using namespace std;
 
@@ -28,6 +30,8 @@ using namespace std;
 
 void init()
 {
+
+
 	circuito = glGenLists(42);
 	glNewList(circuito, GL_COMPILE);
 
@@ -50,9 +54,20 @@ void init()
 	//glColor3f(0.804, 0.522, 0.247);
 	glEndList();
 
+
 	glGenTextures(1, &tex0);
 	glBindTexture(GL_TEXTURE_2D, tex0);
 	loadImageFile("arcoiris.jpg");
+	glEnable(GL_TEXTURE_2D);
+
+	glGenTextures(1, &tex1);
+	glBindTexture(GL_TEXTURE_2D, tex1);
+	loadImageFile("gris.jpg");
+	glEnable(GL_TEXTURE_2D);
+
+	glGenTextures(1, &tex2);
+	glBindTexture(GL_TEXTURE_2D, tex2);
+	loadImageFile("konodioda.jpg");
 	glEnable(GL_TEXTURE_2D);
 
 }
@@ -80,6 +95,67 @@ void display()
 
 	ejes();
 
+	glBindTexture(GL_TEXTURE_2D, tex1);
+	// b. Filtros
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	// c. Forma de combinar y repetir
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+
+	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+
+	GLfloat planoS1[] = { 0,0,0,0 };
+	GLfloat planoT1[] = { -0.1,3,0,0 };
+	glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR);
+	glTexGenfv(GL_S, GL_OBJECT_PLANE, planoS1);
+	glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR);
+	glTexGenfv(GL_T, GL_OBJECT_PLANE, planoT1);
+
+	glPushMatrix();
+	glColor3f(0.0, 0.0, 0.0);
+	glTranslatef(-2.0, 1.75, 10);
+	glScalef(0.1, 3.5, 0.1);
+	glutSolidCube(1.0);
+	glPopMatrix();
+
+	glPushMatrix();
+	glColor3f(0.0, 0.0, 0.0);
+	glTranslatef(2.0, 1.75, 10);
+	glScalef(0.1, 3.5, 0.1);
+	glutSolidCube(1.0);
+	glPopMatrix();
+
+
+	glBindTexture(GL_TEXTURE_2D, tex2);
+	// b. Filtros
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	// c. Forma de combinar y repetir
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+
+
+	glPushMatrix();
+	glColor3f(0.0, 0.0, 0.0);
+	glTranslatef(-2.0, 2.0, 10);
+	glScalef(4.0, 4.0, 1.0);
+	//glutSolidCube(1.0);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0, 0.0);
+	glVertex3f(0.0, 0.0, 0);
+	glTexCoord2f(1.0, 0);
+	glVertex3f(1.0, 0, 0);
+	glTexCoord2f(1.0, 1.0);
+	glVertex3f(1.0, 0.56, 0);
+	glTexCoord2f(0.0, 1.0);
+	glVertex3f(0.0, 0.56, 0);
+	glEnd();
+	glPopMatrix();
+
+
 	glBindTexture(GL_TEXTURE_2D, tex0);
 	// b. Filtros
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
@@ -96,37 +172,28 @@ void display()
 	glTexGenfv(GL_S, GL_OBJECT_PLANE, planoS);
 	glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR);
 	glTexGenfv(GL_T, GL_OBJECT_PLANE, planoT);
-	glPushMatrix();
 
+	glPushMatrix();
 	if (modo == SOLIDO) {
-		
-		//glColor3f(0.545, 0.271, 0.075);
 		glCallList(circuito);
 		glPolygonMode(GL_FRONT, GL_FILL);
-
-
 	}
 	else if (modo == ALAMBRICO) {
-		//glColor3f(0.0, 0.0, 0.0);
-		
 		glCallList(circuito);
 		glPolygonMode(GL_FRONT, GL_LINE);
-
-
 	}
 	else {
-		
 		glPolygonMode(GL_FRONT, GL_LINE);
 		glColor3f(0.0, 0.0, 0.0);
 		glCallList(circuito);
 		glPolygonMode(GL_FRONT, GL_FILL);
-		//glColor3f(0.545, 0.271, 0.075);
 		glCallList(circuito);
-
-
 	}
-
 	glPopMatrix();
+
+	
+
+
 	glFlush();
 	glutSwapBuffers();
 }
